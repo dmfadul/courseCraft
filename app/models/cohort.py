c1c3b1f6a427495392c366cd8ab71a3b28ffb26f
dmfadul <dmf030@gmail.com>
2024-11-13 15:28:36 -0300

    added max to code

--- app/models/cohort.py
+++ app/models/cohort.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import relationship
 class Cohort(db.Model):
     __tablename__ = 'cohorts'
     id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String, nullable=False, unique=True)
+    code = db.Column(db.String(20), nullable=False, unique=True)
     number_of_students = db.Column(db.Integer, nullable=False)
     lunch_position = db.Column(db.Integer, nullable=False)
     theoretical_week_parity = db.Column(db.Integer, nullable=False)
