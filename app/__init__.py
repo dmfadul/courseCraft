ce25b18e0925f25d51dfebf7fa24f98f16f47f76
dmfadul <dmf030@gmail.com>
2024-11-13 14:24:50 -0300

    configured db

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 
 def create_app(config_filename=None):
     config_filename = config_filename or '../instance/config.py'
 
     app = Flask(__name__)
+
     app.config.from_pyfile(config_filename)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.secret_key = app.config['SECRET_KEY']
+
+    db.init_app(app)
 
     return app
 
