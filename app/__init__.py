27aa168ef45e4a84ebcfea28c6ea8e5f23d8bca8
dmfadul <dmf030@gmail.com>
2024-11-13 17:10:01 -0300

    added final login logic and requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 
 def create_app(config_filename=None):
     config_filename = config_filename or '../instance/config.py'
@@ -13,6 +17,13 @@ def create_app(config_filename=None):
     app.secret_key = app.config['SECRET_KEY']
 
     db.init_app(app)
+    bcrypt.init_app(app)
+
+    login_manager.init_app(app)
+    login_manager.login_view = 'login.login'
+    login_manager.login_message = 'Please log in to access this page.'
+    login_manager.login_message_category = 'danger'
+
 
     from app.routes.dashboard.dashboard import dashboard_bp
     from app.routes.grid.grid import grid_bp
--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,12 @@
-from app import db
+from app import db, login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
