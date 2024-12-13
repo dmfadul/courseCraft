b78cacefa214a890fe398989a9ddb4a1f7931e3d
dmfadul <dmf030@gmail.com>
2024-12-13 18:42:12 -0300

    updated lists

--- cli.py
+++ cli.py
@@ -10,32 +10,56 @@ app = create_app()
 wrong_names = []
 for entry in teacher_list:
     code = entry.get("code")
+    t_names = entry.get("teachers")
+    cohorts_str = entry.get("cohorts")
+    cohorts_lst = cohorts_str.split("+")
+    cohorts_lst = [c.strip() for c in cohorts_lst]
+
+    cohorts_lst = ['PAP21' if c == 'PAP' else c for c in cohorts_lst]
+    cohorts_lst = ['DEL44' if c == 'DEL' else c for c in cohorts_lst]
+
+    cohorts_lst = [
+        sub_item
+        for item in cohorts_lst
+        for sub_item in (["APJ04", "APJ05", "APJ06"] if item == "APJ" else [item])
+    ]
 
     with app.app_context():
         discipline = Discipline.query.filter_by(code=code).first()
-
         if discipline is None:
             print(f"Discipline {code} not found.")
             continue
-    
 
-#     t_names = entry.get("teachers")
-#     with app.app_context():
-#         for name in t_names:
-#             if name == "":
-#                 continue
+        for c in cohorts_lst:
+            cohort = Cohort.query.filter_by(code=c).first()
+            if cohort is None:
+                print(f"Cohort {c} not found.")
+                continue
+
+            modulus = Modulus.query.filter_by(discipline_id=discipline.id, cohort_id=cohort.id).first()
+            if modulus is None:
+                print(f"Modulus {code}-{c} not found.")
+                continue
+
+            for name in t_names:
+                if name == "":
+                    continue
         
-#             clean_name = ' '.join([name.capitalize().strip() for name in name.split(' ')])
-#             clean_name = unicodedata.normalize('NFKD', clean_name).encode('ASCII', 'ignore').decode('utf-8')
-#             teacher = Teacher.query.filter_by(name=clean_name).first()
-#             if teacher is None:
-#                 if clean_name not in wrong_names:
-#                     wrong_names.append(clean_name)
-            
-# if wrong_names:
-#     wrong_names = sorted(list(set(wrong_names)))
-#     for i, name in enumerate(wrong_names):
-#         print(i+1, name)
+                clean_name = ' '.join([name.capitalize().strip() for name in name.split(' ')])
+                clean_name = unicodedata.normalize('NFKD', clean_name).encode('ASCII', 'ignore').decode('utf-8')
+                teacher = Teacher.query.filter_by(name=clean_name).first()
+                if teacher is None:
+                    print(f"Teacher {clean_name} not found.")
+                    continue
+
+            #     if teacher not in discipline.teachers:
+            #         print(f"Teacher {teacher.name} not allowed to teach {discipline.name}.")
+            #         continue
+
+                # if teacher not in modulus.teachers:
+                #     flag = modulus.add_teacher(teacher.name)
+                #     print(flag)
+                    
 
 
 
